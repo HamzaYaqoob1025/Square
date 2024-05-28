56e649bf3c17673285844d9b8243d235b2307f2f
Hamza Yaqoob <hamzayaqoob127@gmail.com>
2024-05-28 12:44:35 +0500

    Initial commit

---
+++ Assingment_01/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Assingment_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbinput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(563, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Square";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbinput
+            // 
+            this.tbinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbinput.Location = new System.Drawing.Point(416, 25);
+            this.tbinput.Multiline = true;
+            this.tbinput.Name = "tbinput";
+            this.tbinput.Size = new System.Drawing.Size(259, 39);
+            this.tbinput.TabIndex = 2;
+            this.tbinput.TextChanged += new System.EventHandler(this.tbinput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(144, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Number For Square";
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.BackColor = System.Drawing.Color.White;
+            this.lbloutput.Location = new System.Drawing.Point(416, 155);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(259, 39);
+            this.lbloutput.TabIndex = 5;
+            this.lbloutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(305, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(776, 233);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbinput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbinput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbloutput;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
